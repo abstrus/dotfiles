a1ebb07a4dc0fd4c4bd0a304dbccaf28efbb4966
Charles Bouchard-Légaré <cblegare@users.noreply.github.com>
2017-08-29 22:03:04 -0400

    Install phmyzsh

--- hooks/post-up/02-setup-shell.sh
+++ hooks/post-up/02-setup-shell.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 install_ohmyzsh() {
-        
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"    
+}
+
+main() {
+    install_ohmyzsh
 }
